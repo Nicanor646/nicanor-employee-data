343a721bd7800de070bb4c9bc12dcd56f6de746d
Nicanor646 <nicanor5@gmail.com>
2023-11-07 16:39:17 -0500

    Added port env var to db

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ echo "Building docker"
 docker network create employee_app_net
 
 docker run -d --name employee_data_db \
-           -p 5432:5432 \
+           -p $EMPLOYEE_DB_PORT:5432 \
            -v $PWD/docker_data/db:/var/lib/postgresql/data \
            -e POSTGRES_DB=$EMPLOYEE_DB_NAME \
            -e POSTGRES_USER=$EMPLOYEE_DB_USER \
@@ -20,6 +20,7 @@ docker run -d --name employee_app \
            -e EMPLOYEE_DB_NAME=$EMPLOYEE_DB_NAME \
            -e EMPLOYEE_DB_USER=$EMPLOYEE_DB_USER \
            -e EMPLOYEE_DB_PASSWORD=$EMPLOYEE_DB_PASSWORD \
+           -e EMPLOYEE_DB_PORT=$EMPLOYEE_DB_PORT
            -e EMPLOYEE_DB_HOST=employee_data_db \
            -p 8022:8022 \
            --net employee_app_net \
